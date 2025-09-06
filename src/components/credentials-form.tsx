9555e63c8ab4ca21ee9ac766063a25c2f0bcddd0
ariful97768 <ariful97768@gmail.com>
2025-09-07 01:57:39 +0600

    Add CredentialsForm component and authentication logic

---
+++ src/components/credentials-form.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { useFormStatus } from "react-dom";
+import { authenticate } from "@/lib/actions/auth-action";
+import { useActionState } from "react";
+
+export default function CredentialsForm() {
+  const [state, dispatch] = useActionState(authenticate, undefined);
+  const { pending } = useFormStatus();
+
+  return (
+    <form action={dispatch} className="grid gap-6">
+      <div className="grid gap-3">
+        <Label htmlFor="email">Email</Label>
+        <Input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="email@example.com"
+          required
+        />
+      </div>
+      <div className="grid gap-3">
+        <div className="flex items-center">
+          <Label htmlFor="password">Password</Label>
+          <a
+            href="#"
+            className="ml-auto text-sm underline-offset-4 hover:underline"
+          >
+            Forgot your password?
+          </a>
+        </div>
+        <Input id="password" type="password" name="password" required />
+      </div>
+      {state?.error && (
+        <div className="text-red-600 text-center">{state.error}</div>
+      )}
+      <Button type="submit" className="w-full">
+        {pending ? "Signing in..." : "Sign in"}
+      </Button>
+    </form>
+  );
+}
--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -7,9 +7,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { signIn } from "@/auth";
+import CredentialsForm from "./credentials-form";
 
 export function LoginForm({
   className,
@@ -52,7 +51,6 @@ export function LoginForm({
                 <form
                   action={async () => {
                     "use server";
-
                     await signIn("google");
                   }}
                 >
@@ -72,44 +70,7 @@ export function LoginForm({
                   Or continue with
                 </span>
               </div>
-              <form
-                action={async (formdata) => {
-                  "use server";
-                  await signIn("credentials", formdata);
-                }}
-                className="grid gap-6"
-              >
-                <div className="grid gap-3">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    name="email"
-                    placeholder="email@example.com"
-                    required
-                  />
-                </div>
-                <div className="grid gap-3">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    <a
-                      href="#"
-                      className="ml-auto text-sm underline-offset-4 hover:underline"
-                    >
-                      Forgot your password?
-                    </a>
-                  </div>
-                  <Input
-                    id="password"
-                    type="password"
-                    name="password"
-                    required
-                  />
-                </div>
-                <Button type="submit" className="w-full">
-                  Login
-                </Button>
-              </form>
+              <CredentialsForm />
               <div className="text-center text-sm">
                 Don&apos;t have an account?{" "}
                 <a href="#" className="underline underline-offset-4">
