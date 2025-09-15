02a9d7added4bcc5dbaf195cb7c4f9d44dc2b868
ariful97768 <ariful97768@gmail.com>
2025-09-15 20:12:14 +0600

    Removed createUser and verifySignin function to a separate dedicated folder

--- src/app/api/users/user/route.ts
+++ src/app/api/users/user/route.ts
@@ -1,5 +1,6 @@
-import db from "@/app/database/mongodb";
-import { CreateUserInput, UserResponse, InsertUserOnDB } from "@/lib/types";
+import createUser from "@/lib/backend-actions/create-user";
+import verifySignin from "@/lib/backend-actions/verify-signin";
+import { CreateUserInput } from "@/lib/types";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
@@ -73,108 +74,4 @@ export async function POST(req: NextRequest) {
       { status: 400 }
     );
   }
-}
-
-// made separate functions for better reusability on other cases
-
-export async function createUser(data: CreateUserInput): Promise<UserResponse> {
-  if (!data.name || !data.email) {
-    throw new Error(
-      "Missing required fields. Required: name, email, password."
-    );
-  }
-  if (data.provider === "credentials" && !data.password) {
-    throw new Error("Password is missing for credentials sign in.");
-  }
-
-  const isExist = await db.users.findOne({ email: data.email });
-
-  if (isExist) {
-    throw new Error("User already exits.");
-  }
-
-  let user: InsertUserOnDB;
-  if (data.provider === "credentials") {
-    user = {
-      name: data.name,
-      email: data.email,
-      image: data.image || null,
-      role: data.role || "student",
-      provider: data.provider,
-      password: data.password,
-      isActive: true,
-      createdAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString(),
-    };
-  } else {
-    user = {
-      name: data.name,
-      email: data.email,
-      image: data.image || null,
-      role: data.role || "student",
-      provider: data.provider,
-      isActive: true,
-      createdAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString(),
-    };
-  }
-
-  const result = await db.users.insertOne(user);
-
-  if (result.acknowledged) {
-    return {
-      success: true,
-      message: "User created successfully",
-      data: {
-        id: result.insertedId.toString(),
-        name: user.name,
-        email: user.email,
-        role: user.role,
-        image: user.image,
-        provider: user.provider,
-        isActive: user.isActive,
-        createdAt: user.createdAt,
-        updatedAt: user.updatedAt,
-      },
-    };
-  }
-  throw new Error("Failed to create user.");
-}
-
-export async function verifySignin(credentials: {
-  email: string;
-  password?: string;
-}): Promise<UserResponse> {
-  const userExist = await db.users.findOne({
-    email: credentials.email,
-  });
-
-  if (!userExist) throw new Error("User does not exist.");
-
-  if (userExist.provider === "credentials" && !credentials.password) {
-    throw new Error("Password required for verification.");
-  }
-
-  if (
-    userExist.provider === "credentials" &&
-    userExist.password !== credentials.password
-  ) {
-    throw new Error("Wrong password.");
-  }
-
-  return {
-    success: true,
-    message: "User verification successful.",
-    data: {
-      id: userExist._id.toString(),
-      role: userExist.role,
-      name: userExist.name,
-      email: userExist.email,
-      image: userExist.image,
-      provider: userExist.provider,
-      isActive: userExist.isActive,
-      createdAt: userExist.createdAt,
-      updatedAt: userExist.updatedAt,
-    },
-  };
 }
