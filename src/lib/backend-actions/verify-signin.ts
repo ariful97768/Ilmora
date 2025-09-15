8a96c94ff11a14faffdc52eab7629e59a8e45060
ariful97768 <ariful97768@gmail.com>
2025-09-15 20:10:37 +0600

    Added verify signin function for reusability to verify user log in

---
+++ src/lib/backend-actions/verify-signin.ts
@@ -0,0 +1,41 @@
+import db from "@/app/database/mongodb";
+import { UserResponse } from "../types";
+
+export default async function verifySignin(credentials: {
+  email: string;
+  password?: string;
+}): Promise<UserResponse> {
+
+  const userExist = await db.users.findOne({
+    email: credentials.email,
+  });
+
+  if (!userExist) throw new Error("User does not exist.");
+
+  if (userExist.provider === "credentials" && !credentials.password) {
+    throw new Error("Password required for verification.");
+  }
+
+  if (
+    userExist.provider === "credentials" &&
+    userExist.password !== credentials.password
+  ) {
+    throw new Error("Wrong password.");
+  }
+
+  return {
+    success: true,
+    message: "User verification successful.",
+    data: {
+      id: userExist._id.toString(),
+      role: userExist.role,
+      name: userExist.name,
+      email: userExist.email,
+      image: userExist.image,
+      provider: userExist.provider,
+      isActive: userExist.isActive,
+      createdAt: userExist.createdAt,
+      updatedAt: userExist.updatedAt,
+    },
+  };
+}
