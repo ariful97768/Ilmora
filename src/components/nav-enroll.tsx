3758b3f45c31f6055d189814a71930312564f60f
ariful97768 <ariful97768@gmail.com>
2025-10-25 01:14:28 +0600

    Separated enroll button so that page stays as SSG

---
+++ src/components/nav-enroll.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Link } from "lucide-react";
+import { useSession } from "next-auth/react";
+
+export default function DynamicEnroll() {
+  const session = useSession();
+  return (
+    <>
+      {session && session.data?.user ? (
+        <Link
+          href={"/dashboard/profile"}
+          className="text-lg hover:underline font-bold"
+        >
+          Dashboard
+        </Link>
+      ) : (
+        <Link href={"/login"} className="text-lg hover:underline font-bold">
+          Enroll
+        </Link>
+      )}
+    </>
+  );
+}
