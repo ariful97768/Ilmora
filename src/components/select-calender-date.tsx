0fabb5e67c9d3998872c858e0fa3cdc896771724
ariful97768 <ariful97768@gmail.com>
2025-12-17 18:18:17 +0600

    Added a few shadcn components and update existing ones.

---
+++ src/components/select-calender-date.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import * as React from "react";
+import { ChevronDownIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+export function Calendar22({
+  error,
+  value,
+  onChange,
+}: {
+  error: string | undefined;
+  value: Date | undefined;
+  onChange: React.Dispatch<React.SetStateAction<Date | undefined>>;
+}) {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <div className="flex flex-col gap-3">
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            aria-invalid={error ? true : false}
+            variant="outline"
+            id="date"
+            className="w-full justify-between font-normal"
+          >
+            {value ? value.toLocaleDateString() : "Select date"}
+            <ChevronDownIcon />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+          <Calendar
+            mode="single"
+            selected={value}
+            captionLayout="dropdown"
+            onSelect={(date) => {
+              onChange(date);
+              setOpen(false);
+            }}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
