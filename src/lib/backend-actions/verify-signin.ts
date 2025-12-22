c781c5196835624fce47ceaaf74f333b0b596922
ariful97768 <ariful97768@gmail.com>
2025-12-22 13:30:23 +0600

    - Updated creat-user and verify-user file for better type safety, error handling and database document creation. - Hashed password using bcrypt

--- src/lib/backend-actions/verify-signin.ts
+++ src/lib/backend-actions/verify-signin.ts
@@ -1,39 +1,60 @@
-import db from "@/database/mongodb";
-import { UserResponse } from "../types";
+import getDb from "@/database/mongodb";
+import { UserFromDB } from "../types";
+import bcrypt from "bcryptjs";
 
 export default async function verifySignin(credentials: {
   email: string;
   password?: string;
-}): Promise<UserResponse> {
+}): Promise<
+  { success: true; user: UserFromDB } | { success: false; message: string }
+> {
+  // Get database collections and check if user exist
+  const db = await getDb();
 
   const userExist = await db.users.findOne({
     email: credentials.email,
   });
 
-  if (!userExist) throw new Error("User does not exist.");
+  // No user exists response
+  if (!userExist) return { success: false, message: "User does not exist" };
 
+  // No password provided response
   if (userExist.provider === "credentials" && !credentials.password) {
-    throw new Error("Password required for verification.");
+    return { success: false, message: "Password is required for verification" };
   }
 
-  if (
-    userExist.provider === "credentials" &&
-    userExist.password !== credentials.password
-  ) {
-    throw new Error("Wrong password.");
+  // Verify user credentials and return user data
+  if (userExist.provider === "credentials" && credentials.password) {
+    const isPasswordMatched = await bcrypt.compare(
+      credentials.password,
+      userExist.password
+    );
+    if (!isPasswordMatched)
+      return { success: false, message: "Wrong password" };
+
+    return {
+      success: true,
+      user: {
+        id: userExist._id.toString(),
+        role: userExist.role,
+        email: userExist.email,
+        provider: userExist.provider,
+        status: userExist.status,
+        createdAt: userExist.createdAt,
+        updatedAt: userExist.updatedAt,
+      },
+    };
   }
+  // For non credentials signin such as OAuth sign in, we don't check password
 
   return {
     success: true,
-    message: "User verification successful.",
-    data: {
+    user: {
       id: userExist._id.toString(),
       role: userExist.role,
-      name: userExist.name,
       email: userExist.email,
-      image: userExist.image,
       provider: userExist.provider,
-      isActive: userExist.isActive,
+      status: userExist.status,
       createdAt: userExist.createdAt,
       updatedAt: userExist.updatedAt,
     },
