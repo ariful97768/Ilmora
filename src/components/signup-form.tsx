f7dc9f7eec278385c9df41646a2a973506bafd3f
ariful97768 <ariful97768@gmail.com>
2025-12-23 13:18:41 +0600

    Updated regex as it passed values like "arifulxf@gmai" The dot (.) in the regex was actually checking for any character but it was meant to check for literal dot (.) Fixed by adding a back slash (\) before the dot.

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -111,7 +111,7 @@ export function SignupForm({
                   {...register("email", {
                     required: "Please enter your email address",
                     pattern: {
-                      value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/,
+                      value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
                       message: "Please enter a valid email address",
                     },
                   })}
